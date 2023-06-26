73b42174d318104045d1409f0a971df7383561ae
Bean <122098950+xxbeann@users.noreply.github.com>
2023-06-26 16:35:38 +0900

    Update tensorflow_basic.py

--- tensorflow_basic.py
+++ tensorflow_basic.py
@@ -10,3 +10,10 @@ b = tf.constant([[1.,1.],
                  [0.,1.]])
 c = tf.matmul(a,b) # 내적 곱셈
 print(c)
+
+# 변수: tf.Variable()
+# 초기 값이 미리 설정, 주로 텐서플로 내부에서 연산시 변경되는 변수에 사용 .weigh과 bias
+a = tf.Variable(100)
+b = tf.Variable(200)
+c = tf.add(a,b)
+print(c)
