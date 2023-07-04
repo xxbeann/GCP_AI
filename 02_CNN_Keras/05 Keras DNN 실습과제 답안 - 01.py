d5c923f82e892ede577707eb2cfccdd38feda2eb
Bean <122098950+xxbeann@users.noreply.github.com>
2023-07-04 16:19:45 +0900

    Add files via upload

---
+++ 02_CNN_Keras/05 Keras DNN 실습과제 답안 - 01.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Keras DNN 실습과제 답안 - 01
+
+# In[1]:
+
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+# import csv
+tf.random.set_seed(5)
+np.random.seed(5)
+
+
+# In[2]:
+
+
+boston_train = np.loadtxt('boston_train.csv', delimiter=',', dtype=np.float32, skiprows=1)
+boston_test = np.loadtxt('boston_test.csv', delimiter=',', dtype=np.float32, skiprows=1)
+
+x_train = boston_train[:,:-1]
+y_train = boston_train[:,[-1]]
+print(x_train.shape)   # (400,9)
+print(y_train.shape)   # (400,1)
+
+
+# In[3]:
+
+
+# 모델 구현
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=20, input_shape=[9],activation='relu'),
+    tf.keras.layers.Dense(units=1)
+])
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.summary()
+
+
+# In[4]:
+
+
+# 학습
+history = model.fit(x_train, y_train, epochs=700)
+
+
+# In[5]:
+
+
+# 시각화
+epoch_count = range(1, len(history.history['loss']) + 1)
+plt.plot(epoch_count, history.history['loss'], 'r-')
+plt.legend(['Training Loss'])
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show()
+
+
+# In[6]:
+
+
+# 평가
+x_test = boston_test[:,:9]
+y_test = boston_test[:,[-1]]
+mse = model.evaluate(x_test, y_test)
+# rmse = np.sqrt(mse)
+print('MSE:',mse)
+
+
+# ##  3 layers 
+
+# In[7]:
+
+
+tf.keras.backend.clear_session()
+tf.random.set_seed(5)
+np.random.seed(5) 
+
+
+# In[8]:
+
+
+# 모델 구현
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=20, input_shape=[9],activation='relu'),
+    tf.keras.layers.Dense(units=10, activation='relu'),
+    tf.keras.layers.Dense(units=1)    
+])
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.summary()
+
+
+# In[9]:
+
+
+# 학습
+history = model.fit(x_train, y_train,epochs=700)
+
+
+# In[10]:
+
+
+# 시각화
+epoch_count = range(1, len(history.history['loss']) + 1)
+plt.plot(epoch_count, history.history['loss'], 'r-')
+plt.legend(['Training Loss'])
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show()
+
+
+# In[11]:
+
+
+# 평가
+x_test = boston_test[:,:-1]
+y_test = boston_test[:,[-1]]
+mse = model.evaluate(x_test, y_test)
+# rmse = np.sqrt(mse)
+print('MSE:',mse) 
+# 성능 개선 거의 없음!!
+
+
+# In[ ]:
+
+
+
+
